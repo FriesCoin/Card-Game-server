e736ef632cad3b898928ad551fad115da8d91d33
FriesCoin <96452306+FriesCoin@users.noreply.github.com>
2022-01-15 20:14:02 -0500

    Added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Card_Game_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Servers";
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
